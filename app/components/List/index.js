045a07f6916cef2104e987a1e679ba173796f71d
Steve Butler <stevebutler2210@gmail.com>
2017-12-28 15:03:11 +0000

    Building out the Currency List / generic List Items components

---
+++ app/components/List/index.js
@@ -0,0 +1,5 @@
+import ListItem from "./ListItem";
+import Separator from "./Separator";
+import Icon from "./Icon";
+import styles from "./styles";
+export { ListItem, Separator, Icon, styles };
--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 
-import Home from "./screens/Home";
+import CurrencyList from "./screens/CurrencyList";
 
 EStyleSheet.build({
   $primaryBlue: "#4F6D7A",
   $white: "#FFFFFF",
   $border: "#E2E2E2",
   $inputText: "#797979",
-  $lightGrey: "#F0F0F0"
+  $lightGrey: "#F0F0F0",
+  $darkText: "#343434"
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
