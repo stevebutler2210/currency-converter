a148493ae2bab9509d2b4eade8f5496baa45c64e
Steve Butler <stevebutler2210@gmail.com>
2017-12-28 10:56:18 +0000

    Added 'Reverse Currencies' button and label text

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,11 +4,15 @@ import { StatusBar } from "react-native";
 import { Container } from "../components/Container";
 import { Logo } from "../components/Logo";
 import { InputWithButton } from "../components/TextInput";
+import { ClearButton } from "../components/Buttons";
+import { LastConverted } from "../components/Text";
 
 const TEMP_BASE_CURRENCY = "USD";
 const TEMP_QUOTE_CURRENCY = "GBP";
 const TEMP_BASE_PRICE = "100";
 const TEMP_QUOTE_PRICE = "79.74";
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handlePressBaseCurrency = () => {
@@ -21,6 +25,10 @@ class Home extends Component {
 
   handleTextChange = text => {
     console.log("Change text", text);
+  };
+
+  handleSwapCurrency = () => {
+    console.log("pressed swap currency");
   };
 
   render() {
@@ -40,6 +48,16 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           editable={false}
           defaultValue={TEMP_QUOTE_PRICE}
+        />
+        <LastConverted
+          date={TEMP_CONVERSION_DATE}
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
         />
       </Container>
     );
