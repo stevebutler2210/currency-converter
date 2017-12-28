4d0f3676a1df7e01dda346cbbba4d3dac7352970
Steve Butler <stevebutler2210@gmail.com>
2017-12-28 12:54:18 +0000

    Animating Logo on keyboard open / close

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,23 +4,30 @@ import EStyleSheet from "react-native-extended-stylesheet";
 const imageWidth = Dimensions.get("window").width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+  $largeImageOffset: 15,
+  $smallImageOffset: -30,
+
   container: {
     alignItems: "center"
   },
   containerImage: {
     alignItems: "center",
-    justifyContent: "center",
-    width: imageWidth,
-    height: imageWidth
+    justifyContent: "center"
   },
   image: {
-    width: imageWidth / 2
+    position: "absolute"
   },
   text: {
     color: "$white",
     fontWeight: "600",
     fontSize: 28,
     letterSpacing: -0.5,
-    marginTop: 15
+    marginTop: 15,
+    width: "60%",
+    textAlign: "center"
   }
 });
