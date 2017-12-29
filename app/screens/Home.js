b6f546fff679dd7781ed00180f319a4a2cbf137e
Steve Butler <stevebutler2210@gmail.com>
2017-12-29 00:16:57 +0000

    Conversion Error handling in Home screen

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -9,6 +9,7 @@ import { Logo } from "../components/Logo";
 import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
 import { LastConverted } from "../components/Text";
+import { connectAlert } from "../components/Alert";
 
 import {
   swapCurrency,
@@ -26,11 +27,26 @@ class Home extends Component {
     conversionRate: PropTypes.number,
     isFetching: PropTypes.bool,
     lastConvertedDate: PropTypes.object,
-    primaryColor: PropTypes.string
+    primaryColor: PropTypes.string,
+    alertWithType: PropTypes.func,
+    currencyError: PropTypes.string
   };
 
   componentWillMount() {
     this.props.dispatch(getInitialConversion());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (
+      nextProps.currencyError &&
+      nextProps.currencyError !== this.props.currencyError
+    ) {
+      this.props.alertWithType(
+        "error",
+        "Conversion Error",
+        nextProps.currencyError
+      );
+    }
   }
 
   handlePressBaseCurrency = () => {
@@ -118,8 +134,9 @@ const mapStateToProps = state => {
     lastConvertedDate: conversionSelector.date
       ? new Date(conversionSelector.date)
       : new Date(),
-    primaryColor: state.theme.primaryColor
+    primaryColor: state.theme.primaryColor,
+    currencyError: state.currencies.error
   };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(connectAlert(Home));
