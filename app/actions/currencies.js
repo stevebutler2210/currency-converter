b9943ee2d30ea0bd2e0cf16641692c6237c32f1f
Steve Butler <stevebutler2210@gmail.com>
2017-12-29 00:09:15 +0000

    Implemented data fetching from API using Redux Saga

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -4,6 +4,10 @@ export const CHANGE_BASE_CURRENCY = "CHANGE_BASE_CURRENCY";
 export const CHANGE_QUOTE_CURRENCY = "CHANGE_QUOTE_CURRENCY";
 export const GET_INITIAL_CONVERSION = "GET_INITIAL_CONVERSION";
 
+// These actions relate to the fetching of data from the external API
+export const CONVERSION_RESULT = "CONVERSION_RESULT";
+export const CONVERSION_ERROR = "CONVERSION_ERROR";
+
 export const swapCurrency = () => ({
   type: SWAP_CURRENCY
 });
--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,14 +1,33 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, select, call, put } from "redux-saga/effects";
 
 import {
   CHANGE_BASE_CURRENCY,
   SWAP_CURRENCY,
-  GET_INITIAL_CONVERSION
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR
 } from "../actions/currencies";
 
+const getLatestRate = currency =>
+  fetch(`http://api.fixer.io/latest?base=${currency}`);
+
 function* fetchLatestConversionRates(action) {
-  console.log("Get rates", action);
-  yield;
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getLatestRate, currency);
+    const result = yield response.json();
+
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message });
+  }
 }
 
 export default function* rootSaga() {
